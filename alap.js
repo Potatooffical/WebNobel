a156d98ad80d09a360a0902d982bd92f8917ea9a
Potatooffical <danorb12@gmail.com>
2025-04-16 11:35:26 +0200

    modális kezdet

--- alap.js
+++ alap.js
@@ -6,21 +6,23 @@ function Megjelnit(y) {
     console.log(y);
     let sz="";
     sz+="<div><div class='row'>"
+    let sorszam=0;
     for (const elem of y.prizes) {
         sz+=`
         <div class="col-lg-4">
-        <div class="szegely">
+        <div class="szegely" data-bs-toggle="modal" data-bs-target="#myModal" onclick='reszletkiir(${sorszam})'>
             <p>${elem.year}</p>
             <p>${elem.category}</p>
-            <p>${elem.firstname}</p>
-            <p>${elem.surname}</p>
         </div>
         </div>
 
         `
+        sorszam++;
+       
     }
     sz+="</div></div>"
 document.getElementById("nobel").innerHTML = sz;
-
-
+}
+function reszletkiir(sorszam) {
+    console.log(sorszam);
 }
