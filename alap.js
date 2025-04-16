e6d9163cc575db93faae904cf3372625b268d8cc
Potatooffical <danorb12@gmail.com>
2025-04-16 10:42:09 +0200

    kártyák létrehozzása

--- alap.js
+++ alap.js
@@ -5,18 +5,22 @@ fetch("https://api.nobelprize.org/v1/prize.json")
 function Megjelnit(y) {
     console.log(y);
     let sz="";
+    sz+="<div><div class='row'>"
     for (const elem of y.prizes) {
         sz+=`
-        <div>
-        <p>${elem.year}</p>
-        <p>${elem.category}</p>
+        <div class="col-lg-4">
+        <div class="szegely">
+            <p>${elem.year}</p>
+            <p>${elem.category}</p>
+            <p>${elem.firstname}</p>
+            <p>${elem.surname}</p>
+        </div>
         </div>
 
         `
     }
+    sz+="</div></div>"
 document.getElementById("nobel").innerHTML = sz;
-
-
 
 
 }
