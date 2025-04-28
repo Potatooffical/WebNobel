712e1ad0809a010bf6645d8888953e42d734c84d
Potatooffical <danorb12@gmail.com>
2025-04-28 08:59:34 +0200

    modalisfej kész

--- alap.js
+++ alap.js
@@ -1,15 +1,17 @@
+let tomb=[]
 fetch("https://api.nobelprize.org/v1/prize.json")
     .then(x => x.json())
     .then(y => Megjelnit(y)); 
 
 function Megjelnit(y) {
+    tomb=y
     console.log(y);
     let sz="";
     sz+="<div><div class='row'>"
     let sorszam=0;
     for (const elem of y.prizes) {
         sz+=`
-        <div class="col-lg-4">
+        <div class="col-sm-3">
         <div class="szegely" data-bs-toggle="modal" data-bs-target="#myModal" onclick='reszletkiir(${sorszam})'>
             <p>${elem.year}</p>
             <p>${elem.category}</p>
@@ -24,5 +26,9 @@ function Megjelnit(y) {
 document.getElementById("nobel").innerHTML = sz;
 }
 function reszletkiir(sorszam) {
-    console.log(sorszam);
+    //console.log(sorszam);
+    //alert(JSON.stringify(tomb))
+    //console.log(tomb)
+    document.getElementById("modalfej").innerHTML=tomb.prizes[sorszam].year+" "+tomb.prizes[sorszam].category
+
 }
