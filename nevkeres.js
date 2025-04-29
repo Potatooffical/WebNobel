12dbae1e71dad258f718d79550e081e2fd257ddc
Potatooffical <danorb12@gmail.com>
2025-04-29 09:09:34 +0200

    Név keresés kész

--- nevkeres.js
+++ nevkeres.js
@@ -6,14 +6,52 @@ function keresesnev() {
 }
 
 function nevmegjelenit(y, benev) {
+  let sz = ` 
+<table class="table">
+    <thead>
+      <tr>
+        <th>Year/Category</th>
+        <th>Firstname</th>
+        <th>Surname</th>
+        <th>Motivation</th>
+      </tr>
+    </thead>
+    <tbody>`;
+
   for (const elem of y.prizes) {
     if (elem.laureates != undefined) {
       for (const alelem of elem.laureates) {
-        if (alelem.firstname == benev) {
+        const motivation = alelem.motivation ? alelem.motivation.toLowerCase() : "";
+        if (
+          alelem.firstname.toLowerCase().includes(benev.toLowerCase()) ||
+          alelem.motivation.includes(benev.toLowerCase())
+        ) {
           console.log(alelem.firstname);
+          sz += `<tr>
+          <td>${elem.year} ${elem.category}</td>
+          <td>${alelem.firstname}</td>
+          <td>${alelem.surname}</td>
+          <td>${alelem.motivation || "N/A"}</td>
+          </tr>`;
         }
+        // surname vizsglata külön
+        if (
+           alelem.surname!=undefined && alelem.surname.toLowerCase().includes(benev.toLowerCase())
+          ) {
+            console.log(alelem.firstname);
+            sz += `<tr>
+            <td>${elem.year} ${elem.category}</td>
+            <td>${alelem.firstname}</td>
+            <td>${alelem.surname}</td>
+            <td>${alelem.motivation || "N/A"}</td>
+            </tr>`;
+          }
+
       }
     }
-    else{elem.year+""+elem.category}
   }
+
+  sz += `    </tbody>
+  </table>`;
+  document.getElementById("tablazat").innerHTML = sz;
 }
